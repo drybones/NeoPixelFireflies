cc56c71fed7d14d2b001cfe63d3d1af91cbbf76d
David Bick <david@drybones.co.uk>
2014-11-01 21:35:47 +0000

    First commit

---
+++ NeoPixelFireflies/Firefly.ino
@@ -0,0 +1,50 @@
+Firefly::Firefly () {
+  hueCenter = 0.85;
+  hueRange = 0.2;
+  baseSpeed = 0.02;
+  _brightness = 0.0;
+  _fadeSpeed = 0.0;
+  _isGlowing = false;
+}
+
+void Firefly::ignite() {
+  _hue = hueCenter + (random(100)-50)*0.01*hueRange;
+  _fadeSpeed = baseSpeed*(random(100)*0.01 + 0.5);
+  _isGlowing = true;
+}
+
+boolean Firefly::isGlowing() {
+  return _isGlowing;
+}
+
+void Firefly::animate() {
+  if(!_isGlowing) {
+    return;
+  }
+  
+  _brightness += _fadeSpeed;
+    
+  if(_brightness > 1.0) {
+    _brightness = 1.0;
+    _fadeSpeed *= -0.3;
+  } else if(_brightness < 0.0) {
+    _brightness = 0.0;
+    _fadeSpeed = 0.0;
+    _isGlowing = false;
+  }
+}
+
+uint32_t Firefly::getColor() {
+    uint8_t w = 255*_hue;
+    if(w < 85) {
+      return Adafruit_NeoPixel::Color(w*3*_brightness, (255-w*3)*_brightness, 0);
+    } else if(w < 170) {
+      w -= 85;
+      return Adafruit_NeoPixel::Color((255-w*3)*_brightness, 0, w*3*_brightness);
+    } else {
+      w -= 170;
+      return Adafruit_NeoPixel::Color(0, w*3*_brightness, (255-w*3)*_brightness);
+    }  
+}
+
+
---
+++ NeoPixelFireflies/NeoPixelFireflies.ino
@@ -0,0 +1,101 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+#define NUM_PIXELS 4
+#define MAX_GLOWING_PIXELS 2
+#define MOVE_THRESHOLD 5
+#define STATIONARY_LATENCY 10000 
+
+class Firefly {
+  private:
+    float _brightness;
+    float _fadeSpeed;
+    float _hue;
+    boolean _isGlowing;
+    
+  public:
+    Firefly ();
+	
+    float hueCenter;
+    float hueRange;
+    float baseSpeed;
+    boolean isGlowing();    
+    uint32_t getColor();
+    
+    void ignite();
+    void animate();    
+};
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+
+Firefly ff[NUM_PIXELS];
+boolean moving;
+double lastVector;
+unsigned long lastSensorTime;
+unsigned long lastMovingTime;
+
+void setup() {
+  Serial.begin(9600);
+
+  randomSeed(analogRead(0));
+
+  if (!accel.begin()) {
+    Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
+    while (1);
+  }
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+ 
+  checkMoving();
+  delay(100); // Allow time for the first movement check
+}
+
+void loop() {
+  // Check for movement every 100ms
+  unsigned long time = millis();
+  if(abs(time - lastSensorTime) >= 100) {
+    checkMoving();
+    lastSensorTime = time;
+    if(moving) {
+      lastMovingTime = time;
+    }
+  }
+
+  int glowingPixelCount = 0;
+  // do we have enough lit pixels?
+  for(int i = 0; i < NUM_PIXELS; i++) {
+    if(ff[i].isGlowing()) {
+      glowingPixelCount++;
+    }
+  }
+  
+  for(int i = 0; i < NUM_PIXELS; i++) {
+    if(abs(time - lastMovingTime) <= STATIONARY_LATENCY && !ff[i].isGlowing() && glowingPixelCount < MAX_GLOWING_PIXELS && random(100) < 5) {
+      ff[i].ignite();
+      glowingPixelCount++;
+    }
+    ff[i].animate();
+    strip.setPixelColor(i, ff[i].getColor());
+  }
+  strip.show();
+  delay(10);
+}
+
+void checkMoving() {
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  double newVector = sqrt(event.acceleration.x*event.acceleration.x + event.acceleration.y*event.acceleration.y + event.acceleration.z*event.acceleration.z);
+
+  // are we moving 
+  moving = (abs(newVector - lastVector) > MOVE_THRESHOLD);
+  lastVector = newVector;
+}
+
+
+
+
