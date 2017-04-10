46c351d6741ed36123831ca26a9a119ad2dc3084
David Bick <david@drybones.co.uk>
2017-04-10 16:28:21 +0100

    Simple version without the accelerometer

--- NeoPixelFireflies/NeoPixelFireflies.ino
+++ NeoPixelFireflies/NeoPixelFireflies.ino
@@ -1,13 +1,8 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_LSM303_U.h>
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 6
 #define NUM_PIXELS 7
 #define MAX_GLOWING_PIXELS 4
-#define MOVE_THRESHOLD 1.0 // Quite sensitive
-#define STATIONARY_LATENCY 30000 // 30s glow after last move 
 
 class Firefly {
   private:
@@ -30,42 +25,20 @@ class Firefly {
 };
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
-
 Firefly ff[NUM_PIXELS];
-boolean moving;
-double lastVector;
-unsigned long lastSensorTime;
-unsigned long lastMovingTime;
 
 void setup() {
   Serial.begin(9600);
 
   randomSeed(analogRead(0));
 
-  if (!accel.begin()) {
-    Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
-    while (1);
-  }
-
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
  
-  checkMoving();
   delay(100); // Allow time for the first movement check
 }
 
 void loop() {
-  // Check for movement every 100ms
-  unsigned long time = millis();
-  if(abs(time - lastSensorTime) >= 100) {
-    checkMoving();
-    lastSensorTime = time;
-    if(moving) {
-      lastMovingTime = time;
-    }
-  }
-
   int glowingPixelCount = 0;
   // do we have enough lit pixels?
   for(int i = 0; i < NUM_PIXELS; i++) {
@@ -75,7 +48,7 @@ void loop() {
   }
   
   for(int i = 0; i < NUM_PIXELS; i++) {
-    if(abs(time - lastMovingTime) <= STATIONARY_LATENCY && !ff[i].isGlowing() && glowingPixelCount < MAX_GLOWING_PIXELS && random(100) < 5) {
+    if(!ff[i].isGlowing() && glowingPixelCount < MAX_GLOWING_PIXELS && random(100) < 5) {
       ff[i].ignite();
       glowingPixelCount++;
     }
@@ -85,17 +58,6 @@ void loop() {
   strip.show();
   delay(10);
 }
-
-void checkMoving() {
-  sensors_event_t event; 
-  accel.getEvent(&event);
-  double newVector = sqrt(event.acceleration.x*event.acceleration.x + event.acceleration.y*event.acceleration.y + event.acceleration.z*event.acceleration.z);
-
-  // are we moving 
-  moving = (abs(newVector - lastVector) > MOVE_THRESHOLD);
-  lastVector = newVector;
-}
-
 
 
 
